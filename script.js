7af3e3bd64f6f264ce4e43e0720a07363dfc82ec
Albert Zhan <albert.cfzhan@gmail.com>
2022-03-04 18:04:44 -0800

    calculates expressions with two numbers resets after calculating or clearing

--- script.js
+++ script.js
@@ -13,22 +13,76 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
-        case add:
+        case "+":
             return add(a, b);
-        case subtract:
+        case "-":
             return subtract(a, b);
-        case multiply:
+        case "×":
             return multiply(a, b);
-        case divide:
+        case "÷":
             return divide(a, b);
     }
 }
 
+// display and value functions
+function displayContent(value) {
+    display.textContent += value;
+}
+
+function resetValues() {
+    num1 = null;
+    operator = null;
+    num2 = null;
+    currentNumber = "";
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+
+// TODO: make font of numbers smaller if too big to fit on the display
+// TODO: multiple values
+// TODO: use result of = in next calculation
+
+let num1 = null;
+let operator = null;
+let num2 = null;
+let newCalculation = false;
+let currentNumber = "";
+
 
 const display = document.querySelector('.display');
-
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', (e) => {
-    display.textContent += e.target.textContent;
+    const buttonType = e.target.textContent;
+    if (buttonType == "=") {
+        num2 = currentNumber;
+        clearDisplay();
+        displayContent(operate(operator, num1, num2));
+        resetValues();
+        newCalculation = true;
+    }
+    else if (buttonType == "AC") {
+        resetValues();
+        clearDisplay();
+    }
+    else {
+        if (newCalculation) {
+            clearDisplay();
+            newCalculation = false;
+        }
+        if ((buttonType == "+" || buttonType == "-" || buttonType == "×" || buttonType == "÷") && operator == null) {
+            num1 = currentNumber;
+            currentNumber = "";
+            operator = buttonType;
+        }
+        else {
+            currentNumber += buttonType;
+        }
+        displayContent(buttonType);
+    }
 }));
