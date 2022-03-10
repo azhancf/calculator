8b1c228bee05928f032423342c045ba2fc35b214
Albert Zhan <albert.cfzhan@gmail.com>
2022-03-09 22:35:58 -0800

    attempting to make calculate based on string find entire number work in progress

--- script.js
+++ script.js
@@ -45,38 +45,83 @@ function clearDisplay() {
 
 
 // TODO: make font of numbers smaller if too big to fit on the display
-// TODO: multiple values
 // TODO: use result of = in next calculation
+// TODO: errors if two operators in a row, at the beginning, etc.
+
+
+function findEntireNumber(expression, index, direction) {
+    let currentIndex = index;
+    let currentChar = expression[currentIndex];
+    while (currentChar != "+" && currentChar != "+" && currentChar != "+" && currentChar != "+" && currentIndex != 0 && currentIndex != expression.length - 1) { // or beginning/end of expression
+        direction == "left" ? currentIndex-- : currentIndex++; // index up if right and down if left
+        currentChar = expression[currentIndex];
+    } 
+    // also starts from the operator (doesn't entirely work when past the operator)
+    return expression.substring(currentIndex, index);
+}
+
+function calculate(expression) {
+    for (let i = 0; i < expression.length; i++) {
+        let entireLeft = findEntireNumber(expression, i, "left");
+        console.log(i + ": " + entireLeft);
+        let entireRight = findEntireNumber(expression, i, "right");
+        // switch (expression[i]) {
+        //     case "+":
+        //         console.log(operate("+", entireLeft, entireRight));
+        //         break;
+        //     case "-":
+        //         console.log(operate("-", entireLeft, entireRight));
+        //         break;
+        //     case "×":
+        //         console.log(operate("×", entireLeft, entireRight));
+        //         break;
+        //     case "÷":
+        //         console.log(operate("÷", entireLeft, entireRight));
+        //         break;
+        // }
+    }
+}
 
 
 function recordNumbers(e) {
     const selectedButton = e.target.textContent;
     if (selectedButton == "=") {
-        num2 = currentNumber;
-        clearDisplay();
-        displayContent(operate(operator, num1, num2));
-        resetValues();
-        newCalculation = true;
+        calculate(display.textContent);
     }
     else if (selectedButton == "AC") {
-        resetValues();
         clearDisplay();
     }
     else {
-        if (newCalculation) {
-            clearDisplay();
-            newCalculation = false;
-        }
-        if ((selectedButton == "+" || selectedButton == "-" || selectedButton == "×" || selectedButton == "÷") && operator == null) {
-            num1 = currentNumber;
-            currentNumber = "";
-            operator = selectedButton;
-        }
-        else {
-            currentNumber += selectedButton;
-        }
         displayContent(selectedButton);
     }
+    // redo RESETVALUES function
+
+    // if (selectedButton == "=") {
+    //     num2 = currentNumber;
+    //     clearDisplay();
+    //     displayContent(operate(operator, num1, num2));
+    //     resetValues();
+    //     newCalculation = true;
+    // }
+    // else if (selectedButton == "AC") {
+    //     resetValues();
+    //     clearDisplay();
+    // }
+    // else {
+    //     if (newCalculation) {
+    //         clearDisplay();
+    //         newCalculation = false;
+    //     }
+    //     if ((selectedButton == "+" || selectedButton == "-" || selectedButton == "×" || selectedButton == "÷") && operator == null) {
+    //         num1 = currentNumber;
+    //         currentNumber = "";
+    //         operator = selectedButton;
+    //     }
+    //     else {
+    //         currentNumber += selectedButton;
+    //     }
+    //     displayContent(selectedButton);
+    // }
 }
 
 
