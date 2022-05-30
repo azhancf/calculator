64c69f268ca79642e89ca2425debe6ac91a20843
Albert Zhan <albert.cfzhan@gmail.com>
2022-05-29 17:40:51 -0700

    keyboard suupport for equals, enter, clear rounds to 4 digits with each operation

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
-    return (b != 0 ? (a / b).toFixed(4) : "lmao");   
+    return a / b;
 }
 
 // Operate:
@@ -19,14 +19,28 @@ function operate(operator, a, b) {
     b = Number(b);
     switch (operator) {
         case "+":
-            return add(a, b);
+            return (add(a, b).countDecimals() > 6 ? add(a, b).toFixed(6) : add(a, b));;
         case "-":
-            return subtract(a, b);
+            return (subtract(a, b).countDecimals() > 6 ? subtract(a, b).toFixed(6) : subtract(a, b));;
         case "×":
-            return multiply(a, b);
+            return (multiply(a, b).countDecimals() > 6 ? multiply(a, b).toFixed(6) : multiply(a, b));
         case "÷":
-            return divide(a, b);
+            if (b != 0) {
+                return (divide(a, b).countDecimals() > 6 ? divide(a, b).toFixed(6) : divide(a, b));
+            }
+            return "lmao";
     }
+}
+
+Number.prototype.countDecimals = function () {
+    if (Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    var str = this.toString();
+    if (str.indexOf(".") !== -1 && str.indexOf("-") !== -1) {
+        return str.split("-")[1] || 0;
+    } else if (str.indexOf(".") !== -1) {
+        return str.split(".")[1].length || 0;
+    }
+    return str.split("-")[1] || 0;
 }
 
 // Display and Value Functions:
@@ -48,15 +62,33 @@ function isOperator(char) {
     return char == "+" || char == "-" || char == "×" || char == "÷";
 }
 
+function AC() {
+    resetValues();
+    clearDisplay();
+}
+
+function equals() {
+    if (currentNumber != "" && operator != null) {
+        num2 = currentNumber;
+        clearDisplay();
+        addToDisplay(operate(operator, num1, num2));
+        newCalculation = true;
+        clearDisplayNext = true;
+    }
+}
+
+function backspace() {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
 // todo make decimal points/expression smaller or remove digits to fit on display
 // todo numbers get cleared on second expression after operator
 // todo text is kind of large such as when dividing by 0 or working with large numbers
 // todo disable having two operators in a row
-// todo round even when adding/subtracting
+// todo when rounding, the subtracting doesn't really round correctly
 // todo if there are extra zeroes, get rid of them
 
 
-// todo EXTRA CREDIT: Add keyboard support! (for enter, AC, backspace) [only when certain button]
 
 function clickInput(e) {
     const selectedButtonText = e.target.textContent;
@@ -65,10 +97,8 @@ function clickInput(e) {
 
 function recordNumbers(text) {
     const selectedButtonText = text;
-
     if (selectedButtonText == "AC") {
-        resetValues();
-        clearDisplay();
+        AC();
     }
     else {
         if (newCalculation) {
@@ -76,16 +106,10 @@ function recordNumbers(text) {
             newCalculation = false;
         }
         if (selectedButtonText == "=") {
-            if (currentNumber != "" && operator != null) {
-                num2 = currentNumber;
-                clearDisplay();
-                addToDisplay(operate(operator, num1, num2));
-                newCalculation = true;
-                clearDisplayNext = true;
-            }
+            equals();
         }
         else if (selectedButtonText == "⌫") {
-            display.textContent = display.textContent.slice(0, -1);
+            backspace();
         }
         else {
             if (clearDisplayNext) {
@@ -123,7 +147,18 @@ function recordNumbers(text) {
 
 function keyboardInput(e) {
     const key = document.querySelector(`[data-key="${e.key}"]`);
-    recordNumbers(key.textContent);
+    if (e.key == "Enter") {
+        equals();
+    }
+    else if (e.key == "Tab") {
+        doAC();
+    }
+    else if (e.key == "Backspace") {
+        backspace();
+    }
+    else {
+        recordNumbers(key.textContent);
+    }
 }
 
 function removeTransition() {
